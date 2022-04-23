8955eb454af7d6e82969e52ddb94359d9d5fa3d0
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-04-23 11:48:35 +0700

    Tambah Class

---
+++ Modul8_1302204001/Controllers/MovieController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modul8_1302204001.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+    }
+}
